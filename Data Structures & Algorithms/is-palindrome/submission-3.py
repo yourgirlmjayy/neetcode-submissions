938b5c57020e547216df07e443cc1e9be6e8ea76
Mojolajesu Dada <144645801+yourgirlmjayy@users.noreply.github.com>
2026-09-11 07:57:07 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,20 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+
